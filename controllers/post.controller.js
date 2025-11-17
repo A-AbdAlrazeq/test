4eca2b28fb96341d8fb5c1e55b3d2c4c29301934
Abd Alrahman Abd Alrazeq <a.abdalrazeq@outlook.com>
2025-11-17 13:57:04 +0200

    add update and delete for post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,9 +2,21 @@ import Post from "../models/post.model.js";
 
 export const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find();
+   /*  const posts = await Post.find({
+    $or: [
+      { is_deleted: false },
+      { is_deleted: { $exists: false } }
+    ]
+  })
     res.json(posts);
-  } catch (err) {
+  } */
+  const posts = await Post.find().or([
+    { is_deleted: false },
+    { is_deleted: { $exists: false } }
+  ])
+    res.json(posts);
+}
+   catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
@@ -29,5 +41,30 @@ export const getPostById = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    post.is_deleted = true;
+    post.deleted_at = new Date();
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-export default { getPosts, createPost, getPostById };
+export default { getPosts, createPost, getPostById,updatePost,deletePost };
